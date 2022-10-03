728f1219765b5981a777db2070bb6031373fa020
Federico Tensi <federicotensi@gmail.com>
2022-10-03 16:39:15 +0200

    component search, form validation hookform and yup

--- carburante-react/src/App.js
+++ carburante-react/src/App.js
@@ -1,9 +1,48 @@
+import React, { useState } from 'react';
+
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 import './App.css';
 
+// Components
+import Search from './components/Search';
+
 function App() {
+
+
+  // const [getFormData, setFormDataValues] = useState('');
+  const [formDataValues, setFormDataValues] = useState('');
+
+  const getFormData = (data) => {
+    setFormDataValues(data);
+    console.log(formDataValues)
+  }
+
   return (
-    <></>
+    <div>
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#">⛽Costo Carburante</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+
+            </Nav>
+            <Nav>
+              <Nav.Link href="https://www.tensi.dev">Home site</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      {/* Add main search components */}
+      <Search getFormData={getFormData} />
+
+      {/* Add result components */}
+      {/* Add statistics component */}
+    </div>
   );
 }
 
---
+++ carburante-react/src/components/Search.js
@@ -0,0 +1,169 @@
+import Container from 'react-bootstrap/Container';
+import Card from 'react-bootstrap/Card';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import { useEffect, useState } from 'react';
+
+
+// Validation
+import { useForm } from 'react-hook-form';
+import * as Yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+
+function Search({ getFormData }) {
+    const [lat, setLat] = useState(null);
+    const [lng, setLng] = useState(null);
+    const [gpsStatus, setGpsStatus] = useState(null);
+    const [isInputDisabled, setIsInputDisabled] = useState(true);
+
+    // Form validation
+    const yupValidation = Yup.object().shape({
+        carburante: Yup.mixed().required('🙏🏻 Perfavore, seleziona un tipo di carburante').notOneOf(['null', 'Tipo di Carburante']),
+        ordineprezzo: Yup.mixed().required('🙏🏻 Perfavore, seleziona l\'ordine di visualizzazione prezzi.').notOneOf(['null', 'Ordine Prezzo']),
+        distanzaricerca: Yup.number().required('🙏🏻 Perfavore, inserisci la distanza di ricerca.'),
+    })
+    const formOptions = { resolver: yupResolver(yupValidation) }
+    const { register, handleSubmit, reset, formState } = useForm(formOptions)
+    const { errors } = formState
+
+    function onSubmit(data) {
+        return getFormData(data)
+        // return setFormDataValues(JSON.stringify(data, null, 4))
+
+    }
+
+
+
+
+    // Called upon component mount
+    useEffect(() => {
+        getLocation();
+    }, []);
+
+
+    const getLocation = () => {
+        if (!navigator.geolocation) {
+            setGpsStatus('Geolocation is not supported by your browser');
+        } else {
+            setGpsStatus('Recuperando...');
+            navigator.geolocation.getCurrentPosition((position) => {
+                setIsInputDisabled(false)
+                setGpsStatus('📍Posizione recuperata!');
+                setLat(position.coords.latitude);
+                setLng(position.coords.longitude);
+            }, () => {
+                setGpsStatus('Impossibile recuperare la tua posizione');
+            }, { maximumAge: 5000, timeout: 50000, enableHighAccuracy: true });
+        }
+
+    }
+
+    // function onSubmit(data) {
+    //     console.log(JSON.stringify(data, null, 4))
+    // }
+
+
+    return (
+        <Container className="mt-3">
+            <Card>
+                <Card.Body>
+                    <Card.Title>
+                        Ricerca
+                        {/* <Row >
+                            <Col sm={8}>Ricerca</Col>
+
+                        </Row> */}
+
+                    </Card.Title>
+
+                    <Card.Subtitle className="mb-2 text-muted fs-6">
+                        <p className="fs-6">{gpsStatus}</p>
+                        <hr />
+                    </Card.Subtitle>
+                    <Container>
+                        <form onSubmit={handleSubmit(onSubmit)}>
+                            <Row>
+                                <Col>
+
+                                    <Form.Select size="sm" disabled={isInputDisabled} name="carburante" className={`form-control ${errors.carburante ? 'is-invalid' : ''}`}
+                                        {...register('carburante')}>
+                                        <option defaultValue="none" selected disabled>Tipo di Carburante</option>
+                                        <option value="1-x">Benzina (Tutti)</option>
+                                        <option value="1-1">Benzina (Self)</option>
+                                        <option value="1-0">Benzina (Servito)</option>
+                                        <option disabled value="none"></option>
+                                        <option value="2-x">Gasolio (Tutti)</option>
+                                        <option value="2-1">Gasolio (Self)</option>
+                                        <option value="2-0">Gasolio (Servito)</option>
+                                        <option disabled value="none"></option>
+                                        <option value="3-x">Metano (Tutti)</option>
+                                        <option value="3-1">Metano (Self)</option>
+                                        <option value="3-0">Metano (Servito)</option>
+                                        <option disabled value="none"></option>
+                                        <option value="4-x">GPL (Tutti)</option>
+                                        <option value="4-1">GPL (Self)</option>
+                                        <option value="4-0">GPL (Servito)</option>
+                                        <option disabled value="none"></option>
+                                        <option value="323-x">L-GNC (Tutti)</option>
+                                        <option value="323-1">L-GNC (Self)</option>
+                                        <option value="323-0">L-GNC (Servito)</option>
+                                        <option disabled value="none"></option>
+                                        <option value="324-x">GNL (Tutti)</option>
+                                        <option value="324-1">GNL (Self)</option>
+                                        <option value="324-0">GNL (Servito)</option>
+
+                                    </Form.Select>
+                                    <small className="invalid-feedback">{errors.carburante?.message}</small>
+                                    {/* <small className="form-text text-danger visually-hidden">🙏🏻 Perfavore, seleziona un tipo di carburante.</small> */}
+                                </Col>
+                                <Col>
+                                    <Form.Select size="sm" disabled={isInputDisabled} name="ordineprezzo" className={`form-control ${errors.ordineprezzo ? 'is-invalid' : ''}`}
+                                        {...register('ordineprezzo')}>
+                                        <option defaultValue="none" selected disabled>Ordine Prezzi</option>
+                                        <option value="asc">📈 Crescente</option>
+                                        <option value="desc">📉 Decrescente</option>
+                                    </Form.Select>
+                                    <small className="invalid-feedback">{errors.ordineprezzo?.message}</small>
+                                    {/* <small className="form-text text-danger visually-hidden">🙏🏻 Perfavore, seleziona l'ordine di visualizzazione prezzi.</small> */}
+                                </Col>
+                            </Row>
+                            <Row className="mt-3">
+                                <Col>
+                                    <Form.Select size="sm" disabled={isInputDisabled} name="distanzaricerca" className={`form-control ${errors.distanzaricerca ? 'is-invalid' : ''}`}
+                                        {...register('distanzaricerca')}>
+                                        <option defaultValue="none" selected disabled>Distanza di Ricerca</option>
+                                        <option value="1000"> 1Km (🆘)
+                                        </option>
+                                        <option value="3000"> 3Km (😱)
+                                        </option>
+                                        <option value="5000"> 5Km (😨)
+                                        </option>
+                                        <option value="10000"> 10Km (😐)
+                                        </option>
+                                        <option value="20000"> 20Km (🤷🏻‍♂️)
+                                        </option>
+                                        <option value="50000"> 50Km (👋🏼)
+                                        </option>
+
+                                    </Form.Select>
+                                    {/* <small className="form-text text-danger">🙏🏻 Perfavore, seleziona la distanza di ricerca.</small> */}
+                                    <small className="invalid-feedback">{errors.distanzaricerca?.message}</small>
+                                </Col>
+                            </Row>
+                            <div className="mx-auto btn-lg text-center pt-3">
+                                <Button type="submit" variant="outline-primary" disabled={isInputDisabled} >Ricerca 🔎</Button>
+                            </div>
+                        </form>
+                    </Container>
+                    <Card.Link href="#">Card Link</Card.Link>
+                    <Card.Link href="#">Another Link</Card.Link>
+                </Card.Body>
+            </Card>
+
+        </Container>
+    )
+}
+
+export default Search
